bac2c5ec08856f5137b1dd61a995ac01f8b25a26
Yasai-web <weissrot.k@gmail.com>
2026-04-29 12:09:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ function showPopup(
   `;
 
   popup.style.left = `${event.clientX}px`;
-  popup.style.top = `${event.clientY + 100}px`;
+  popup.style.top = `${event.clientY + 80}px`;
 
   popup.classList.add("show");
 
